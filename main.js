c1d75caccee21eedcc17a2f76509c5e1f851b43b
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-05-02 20:45:37 -0500

    ToDo: connect selector, id #'s

--- main.js
+++ main.js
@@ -1,95 +1,81 @@
 "use strict";
-let products = [];
-let types = [];
-let categories = [];
+(function(){  
+    var allData = [];
+    $( document ).ready(function() {
+        // CALLING AND THEN DISPLAYING PRODUCTS // 
+        var callProducts = new Promise(function(resolve){ // PRACTICE WITH REJECT 2ND ARG OR .CATCH METHOD
+            $.ajax({
+                url: "products.json"
+            }).done(function(productsData){
+                resolve();
+                allData.push(productsData.products[0]); 
+            });
+        }); 
+        callProducts
+            .then(callTypes) 
+            .then(callCategories)
 
-var buildProducts = new Promise(function(resolve, reject){
-    $.ajax({
-        url: "products.json"
-    }).done(function(productsData){
-        resolve(productsData.products[0]);
-    });
-}); 
-buildProducts
-    .then(doSomething);
-function doSomething(productArray){
-    console.log("products: ", productArray);
-};
+        // CALLING TYPES //
+        var callTypes = new Promise(function(resolve){
+            $.ajax({
+                url: "types.json"
+            }).done(function(typesData) {
+                resolve();
+                allData.push(typesData.types);
+                console.log(typesData.types[4].description);
+                // console.log(typesData);
+                // console.log(allData[0], allData[1], allData[2]);
+            });
+        });
 
-var buildCategories = new Promise(function(resolve, reject){
-    $.ajax({
-        url: "categories.json"
-    }).done(function(categoriesData){
-        resolve(categoriesData.categories);
-    });
-}); 
-buildCategories
-    .then(showCategories);
-function showCategories(categoriesArray){
-    console.log("products: ", categoriesArray);
-};
+        // CALLING CATEGORIES // 
+        var callCategories = new Promise(function(resolve){
+            $.ajax({
+                url: "categories.json"
+            }).done(function(categoriesData){
+                resolve(categoriesData);
+                allData.push(categoriesData.categories);
+                // console.log(allData[0], allData[1], allData[2]);
+            });
+        }); 
+        callCategories
+            .then(toDom)
 
-var buildTypes = new Promise(function(resolve, reject){
-    $.ajax({
-        url: "types.json"
-    }).done(function(typesData) {
-        resolve(typesData);
-    });
-});
-buildTypes
-    .then(addTypes)
+        function toDom() {
+            console.log(allData[0], allData[1], allData[2]);
+            console.log(allData);
+            for (var product in allData[0]) {
+                var string = "";
+                var productKey = allData[0][product];
+                string += "<div class='col-md-2 card'>";
+                string += `<p class="badge category">Category: ${productKey.type}</p>`; // PATCH IN CATEGORIES
+                string += `<p class="badge typeName">Type: ${productKey.type}</p>`; // PATCH IN TYPES
+                string += `<p class="productName">${productKey.name}</p>`;
+                string += `<p>${productKey.description}</p>`;
+                string += `<p class="badge productID">ID number: ${productKey.id}</p>`; // PRODUCT ID. NOT SURE I NEED IT. 
+                string += "</div>";
+                $("#output").append(string);
+            }
+            // $(".category").html(allData[0].susan.id);
+            replaceStuff();
+        };
 
-function addTypes(typesData) {
-    for (type in typesData.types) {
-        var string = "";
-        var type = typesData.types[type];
-        string += "<div class='col-md-6 card'>";
-        string += `<p>${type.name}</p>`;
-        string += `<p>${type.description}</p>`;
-        string += "</div>";
-        $("#output").append(string);
-    }
-};
+        function replaceStuff() {
+            if ($(".typeName").html("1")) {
+             $(".typeName").replaceWith("<p class='badge typeName'>Funky Town</p>");
+            };
+        };
 
-$("#selector").change(function(){
-    if ($("#selector").val()==="Demolition") {
-        console.log("demo!");
-        // Call function to load demos;
-    } else if ($("#selector").val() === "Fireworks") {
-        console.log("fuego trabajos");
-        // call function to load fireworks;
-    };
-})
+        // SELECTOR THINGY // 
+        $("#selector").change(function(){
+            if ($("#selector").val()==="Demolition") {
+                console.log("demo!");
+                // Call function to load demos;
+            } else if ($("#selector").val() === "Fireworks") {
+                console.log("fuego trabajos");
+                // call function to load fireworks;
+            };
+        });
 
-
-/* -------------------- playing around with TreeHouse stuff ------------------- */
-
-// var calculationPromise = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         resolve(1 + 1);
-//     }, 1000);
-// });
-
-// var calculationPromise2 = new Promise(function(resolve, reject){
-//     setTimeout(function(){
-//         resolve(1+2);
-//     }, 500);
-// });
-
-// function addTwo(value){
-//     return value + 2;
-// }
-
-// function printFinalValue(nextValue){
-//     console.log("The final value is ", nextValue);
-// }
-
-// calculationPromise
-//     .then(addTwo)
-//     .then(addTwo)
-//     .then(printFinalValue);
-
-// calculationPromise2
-//     .then(addTwo)
-//     .then(addTwo)
-//     .then(printFinalValue);
+    }); // <---- .READY() 
+})(); // <----- IIFE 
